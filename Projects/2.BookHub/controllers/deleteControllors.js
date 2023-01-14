2e5b26442d329987f437d3920ee8fd87c90b06d7
Alfonso <fonsovargas81@gmail.com>
2023-01-13 21:50:51 -0600

    linked books for display

--- Projects/2.BookHub/controllers/deleteControllors.js
+++ Projects/2.BookHub/controllers/deleteControllors.js
@@ -1,9 +1,10 @@
 const {bookModel} = require('../models/bookModel')
 
-exports.displayDeletePage = (req,res) => res.render('deleteBooks.ejs')
-exports.getAllBooks = (req, res) => {
+exports.displayDeletePage = (req,res) => {
     bookModel.find({}, (err, bookArray) => {
-      if (err) res.render("results.ejs", { message: err.message });
-      else res.render("readBooks.ejs", { books: bookArray });
+        if (err) res.render("results.ejs", { message: err.message });
+            else res.render("deletebooks.ejs", { books: bookArray });
     });
-  };
+};
+
+
